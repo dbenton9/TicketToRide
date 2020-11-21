9b64a9e48f7ae302b409bcac9c6594e6083eddc4
Shelby Benton <shelby.benton@cru.org>
2020-11-21 10:41:41 -0500

    testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,7 @@ $("#completedRoutes").keyup(function(){
         inputBox.attr("value", 0);
         inputBox.attr("id", "completed"+i);
         inputBox.attr("onClick", "this.select()");
+        inputBox.attr("type", "")
         inputBox.attr("pattern", "[0-9]*")
     }
 });
